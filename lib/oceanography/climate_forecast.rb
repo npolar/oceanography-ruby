095de4d894b2e445bded697cc81aeb5c51c8d03c
cnrdh <cnrdh@users.noreply.github.com>
2014-10-14 17:36:52 +0200

    Added

---
+++ lib/oceanography/climate_forecast.rb
@@ -0,0 +1,27 @@
+module Oceanography
+  
+  class ClimateForecast
+    
+    # Climate and forecast conventions variable and attribute mappper for common synonyms 
+    def self.mapper
+      lambda {|hash|
+        mapped = {}
+        hash.each do |k,v|
+          key = case k
+            when /^(temperature|temp|t)$/ui
+              "sea_water_temperature"
+            when /^(pressure|pres|p)$/ui
+              "sea_water_pressure"
+            else k.downcase
+          end
+          
+          mapped[key]=v
+          
+          
+        end
+        mapped
+      }
+    end
+    
+  end  
+end
